fdaa6c82c34b952f7b434fb33e447c1ca7d3595b
phasuwut <44774302+phasuwut@users.noreply.github.com>
2021-09-22 01:32:56 +0700

    update code

--- test/server.py
+++ test/server.py
@@ -1,15 +1,25 @@
 from socket import *
+import time
 
 serverPort = 2500
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
 
+def convertTuple(tup):
+    st = ''.join(map(str, tup))
+    return st
+
 while 1:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    """ print(serverSocket.recvfrom(2048)) """
-    print(serverSocket)
-    print(message)
-    print(clientAddress)
-    modifiedMessage = message.upper() 
-    serverSocket.sendto(modifiedMessage, clientAddress)
+    time_string = time.asctime()
+    print(time_string)
+
+    message, clientAddress,  = serverSocket.recvfrom(2048)
+    print("client sent => ",message)
+    time.sleep(1)
+
+    clientAddressString = convertTuple(clientAddress)
+    messageOtput=time_string+" server'Address :=> 127.0.0.1:"+ str(serverPort) +", Your'Address :=> " + clientAddressString
+    
+    serverSocket.sendto(messageOtput.encode('utf-8'), clientAddress)
+
