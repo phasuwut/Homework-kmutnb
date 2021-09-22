a39a76d07c8ae059b4f073ac38d049fc67362f5a
phasuwut <44774302+phasuwut@users.noreply.github.com>
2021-09-23 01:50:25 +0700

    add code TCP

--- TCP/test/client.py
+++ TCP/test/client.py
@@ -1,11 +1,44 @@
 from socket import *
+
+import time
+from itertools import count
+from multiprocessing import Process
+
+counter = count(0)
 serverName = "127.0.0.1"
 serverPort = 12000
 
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort)) 
-sentence = input("Input lowercase sentence:")
-clientSocket.send(sentence.encode('utf-8'))
-modifiedSentence = clientSocket.recv(1024) 
-print ("From Server: ", modifiedSentence)
-clientSocket.close()
+def sentTCP(x):
+    message = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    print("times : ",x)
+    print("I send message to 127.0.0.1:2500")
+    print("It says => ",message)
+
+    try:
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        clientSocket.connect((serverName,serverPort)) 
+        clientSocket.send(message.encode('utf-8'))
+        modifiedSentence = clientSocket.recv(1024) 
+        print ("From Server: ", modifiedSentence)
+        clientSocket.close()
+    except Exception as e: 
+        print("error")
+        print(e)
+    print(" ")
+
+def runTCP():
+    time.sleep(1)
+    for x in range(0, 3):
+        sentTCP(x)
+        next(counter)
+
+p1 = Process(target=runTCP, name='Process_inc_forever')
+
+if __name__ == '__main__':
+
+    p1.start()
+    p1.join(timeout=5)
+    if p1.exitcode is None:
+        print(f'Oops, {p1} timeouts!')
+        p1.terminate()
+ 
--- TCP/test/server.py
+++ TCP/test/server.py
@@ -1,12 +1,32 @@
 from socket import *
+import time
+
 serverPort = 12000
+serverIp = '127.0.0.1'
+
 serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(("",serverPort))
+serverSocket.bind((serverIp,serverPort))
 serverSocket.listen(1)
 print ("The server is ready to receive")
+
+def convertTuple(tup):
+    st = ':'.join(map(str, tup))
+    return st
+
+
 while 1:
     connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024) 
-    capitalizedSentence = sentence.upper() 
-    connectionSocket.send(capitalizedSentence) 
+    message = connectionSocket.recv(1024) 
+    
+    """ time.sleep(3) """
+
+    clientAddressToString = convertTuple(addr)
+    time_string = time.asctime()
+    print("client sent => ",message)
+    print(time_string)
+    print(clientAddressToString)
+    print("")
+    messageOtput=time_string+" server'Address :=> 127.0.0.1:"+ str(serverPort) +", Your'Address :=> " + clientAddressToString
+    
+    connectionSocket.send(messageOtput.encode('utf-8')) 
     connectionSocket.close()
