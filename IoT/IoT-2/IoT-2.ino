05e1d8abf64d9d1b3c9f350007985a3002c35393
phasuwut <44774302+phasuwut@users.noreply.github.com>
2022-04-04 19:28:59 +0700

    update

--- IoT/IoT-2/IoT-2.ino
+++ IoT/IoT-2/IoT-2.ino
@@ -1,22 +1,11 @@
-#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
- 
-// lcd
-LiquidCrystal_I2C lcd(0x27, 20, 4);
 
+#include <Wire.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
 
 int pin_pir_input=0;
 int pin_pir_output=2;
 int pin_buzzer=12;
-
-int pin_LED_IN=13;
-int pin_LED_OUT=15;
-
-int valInput = 0;
-int valOutput = 0;
-
-int dataInput=0;
-int dataOutput=0;
 
 int count=0;
 void setup() {
@@ -24,9 +13,6 @@ void setup() {
   pinMode(pin_pir_output, INPUT);  
   
   pinMode(pin_buzzer, OUTPUT); // sets the pin as output
-
-  pinMode(pin_LED_IN, OUTPUT); // sets the pin as output
-  pinMode(pin_LED_OUT, OUTPUT); // sets the pin as output
 
   lcd.begin();
   // Turn on the blacklight and print a message.
@@ -36,39 +22,5 @@ void setup() {
 }
    
 void loop() {
-  valInput = digitalRead(pin_pir_input); 
-  valOutput = digitalRead(pin_pir_output); 
-
-
-    if (valInput == 1) { // ค่า เป็น 0 ตรวจจับเจอวัตถุ สั่งให้ไฟ LED ติด
-        beep(500);
-        count=count+1;
-        dataInput=1;
-    }
-
-    lcd.setCursor(0, 0); // ไปที่ตัวอักษรที่ 0 แถวที่ 1
-    lcd.print("count");
-    lcd.setCursor(6, 1); // ไปที่ตัวอักษรที่ 6 แถวที่ 2
-    lcd.print(count);
-
-  //delay(100);
-}
-
-
-
-
-// buzzer
-void beep(unsigned char delayms){
-  analogWrite(pin_buzzer, 20); // Almost any value can be used except 0 and 255
-  // experiment to get the best tone
-  delay(delayms); // wait for a delayms ms
-  analogWrite(pin_buzzer, 0); // 0 turns it off
-  delay(delayms); // wait for a delayms ms
-}
-
-void openBuzzer(){
-  tone(pin_buzzer, 500);
-  delay(500);
-  tone(pin_buzzer, 1000);
-  delay(500);
+ 
 }
---
+++ IoT/project/sketch_apr04a/sketch_apr04a.ino
@@ -0,0 +1,77 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+ 
+// Set the LCD address to 0x27 or 0x3F for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+int pin_pir_input=0;
+int pin_pir_output=2;
+int pin_buzzer=12;
+
+int pin_LED_IN=13;
+int pin_LED_OUT=15;
+
+int valInput = 0;
+int valOutput = 0;
+
+int dataInput=0;
+int dataOutput=0;
+
+int count=0;
+
+void setup(){
+  pinMode(pin_pir_input, INPUT);
+  pinMode(pin_pir_output, INPUT);  
+  pinMode(pin_buzzer, OUTPUT); // sets the pin as output
+
+  pinMode(pin_LED_IN, OUTPUT); // sets the pin as output
+  pinMode(pin_LED_OUT, OUTPUT); // sets the pin as output
+  
+  // initialize the LCD
+  lcd.begin();
+  // Turn on the blacklight and print a message.
+  lcd.backlight();
+
+  
+  Serial.begin(9600);
+ 
+}
+void loop() {
+  valInput = digitalRead(pin_pir_input); 
+  valOutput = digitalRead(pin_pir_output); 
+
+
+  if (valInput == 1) { // ค่า เป็น 0 ตรวจจับเจอวัตถุ สั่งให้ไฟ LED ติด
+    //beep(500);
+    count=count+1;
+    dataInput=1;
+    digitalWrite(pin_LED_IN, HIGH);// LED on
+    delay(100);
+  }else{
+    digitalWrite(pin_LED_IN, LOW); // LED off
+  }
+
+
+  // LED
+  lcd.setCursor(0, 0); // ไปที่ตัวอักษรที่ 0 แถวที่ 1
+  lcd.print("Welcome To");
+  lcd.setCursor(6, 1); // ไปที่ตัวอักษรที่ 6 แถวที่ 2
+  lcd.print(valInput);
+ 
+}
+
+// buzzer
+void beep(unsigned char delayms){
+  analogWrite(pin_buzzer, 20); // Almost any value can be used except 0 and 255
+  // experiment to get the best tone
+  delay(delayms); // wait for a delayms ms
+  analogWrite(pin_buzzer, 0); // 0 turns it off
+  delay(delayms); // wait for a delayms ms
+}
+
+void openBuzzer(){
+  tone(pin_buzzer, 500);
+  delay(500);
+  tone(pin_buzzer, 1000);
+  delay(500);
+}
